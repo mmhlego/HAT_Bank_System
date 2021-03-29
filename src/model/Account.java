a540017b7243c787c83710e495f28e4970fcd1da
mmhlego <mmhlego@gmail.com>
2021-03-29 05:48:46 +0430

    DBConnector completed

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,16 +1,17 @@
 package model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Account {
-    String OwnerID, IBAN, CVV, CVV2, AccountID;
-    Date ExDate;
+    String OwnerID, BIC, IBAN, CVV, CVV2, AccountID;
+    LocalDate ExDate;
     int Status;
     long Value;
 
-    public Account(String ownerID, String iban, String cvv, String cvv2, Date exDate, int status, long value,
-            String accountID) {
+    public Account(String ownerID, String bic, String iban, String cvv, String cvv2, LocalDate exDate, int status,
+            long value, String accountID) {
         OwnerID = ownerID;
+        BIC = bic;
         IBAN = iban;
         CVV = cvv;
         CVV2 = cvv2;
@@ -18,5 +19,29 @@ public class Account {
         ExDate = exDate;
         Status = status;
         Value = value;
+    }
+
+    public static String generateID() {
+        int all = DBConnector.numberOfAccounts();
+
+        String ID = "A-";
+        String number = Integer.toString(all + 1);
+        for (int i = 0; i < (5 - number.length()); i++) {
+            ID += "0";
+        }
+        ID += number;
+
+        return ID;
+    }
+
+    public static String generateID(int n) {
+        String ID = "A-";
+        String number = Integer.toString(n);
+        for (int i = 0; i < (5 - number.length()); i++) {
+            ID += "0";
+        }
+        ID += number;
+
+        return ID;
     }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -13,9 +13,8 @@ public class User {
     public ArrayList<Loan> Loans = new ArrayList<Loan>();
     public ArrayList<Account> Accounts = new ArrayList<Account>();
 
-    public User(String firstName, String lastName, String username, String password, String email, String phone,
-            int accessLevel, String address, String id, String nationalCode, LocalDate birthDate, int theme,
-            int language) {
+    public User(String firstName, String lastName, String username, String password, int accessLevel, String address,
+            String id, String nationalCode, LocalDate birthDate, String email, String phone, int theme, int language) {
         FirstName = firstName;
         LastName = lastName;
         Username = username;
@@ -55,6 +54,19 @@ public class User {
         String ID = alph[AccessLevel] + "-";
         String number = Integer.toString(SameAccessUsersCount + 1);
         for (int i = 0; i < (5 - number.length()); i++) {
+            ID += "0";
+        }
+        ID += number;
+
+        return ID;
+    }
+
+    public static String generateID(int AccessLevel, int number) {
+        // (C/E/M)-(00001-99999)
+        String[] alph = { "C", "E", "M" };
+        String ID = alph[AccessLevel] + "-";
+        String num = Integer.toString(number);
+        for (int i = 0; i < (5 - num.length()); i++) {
             ID += "0";
         }
         ID += number;
