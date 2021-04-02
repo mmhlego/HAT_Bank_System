ed01cc69f5441ce77d175cff52556ef6bf1426a1
mmhlego <mmhlego@gmail.com>
2021-04-02 12:35:30 +0430

    Fixed Account page in E & M

--- src/controller/AccountsShow.java
+++ src/controller/AccountsShow.java
@@ -33,58 +33,89 @@ public class AccountsShow implements Initializable {
 		int i = 0;
 		for (Account account : accounts) {
 			FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/cards.fxml"));
-			try {
-				AnchorPane cardPage = loader.load();
-				AnchorPane card = (AnchorPane) cardPage.getChildren().get(0);
 
-				cardCreator creator = loader.getController();
-				creator.getCardNumber().setText(account.BIC);
-				creator.getCvv2().setText(account.CVV2);
-				creator.getExp().setText(account.ExDate.getYear() + "/" + account.ExDate.getMonthValue());
-				creator.getIban().setText(account.IBAN);
-				card.setLayoutX((25 + (i % 2) * 420));
-				card.setLayoutY((25 + i / 2 * 225));
-				card.setCursor(Cursor.HAND);
-				card.setOnMouseClicked(e -> {
-					FXMLLoader load = new FXMLLoader(getClass().getResource("../view/AccountInformation.fxml"));
-					Parent parent;
-					try {
-						parent = load.load();
-						AccountInformation control = load.getController();
-						if (!DBConnector.IsCardAlive(account.ExDate.getYear(), account.ExDate.getMonthValue())) {
-							Alert alert = new Alert(AlertType.ERROR);
-							alert.setTitle("Expired");
-							alert.setHeaderText("Your Card is Expired");
-							alert.show();
-							control.getDepositBTn().setDisable(true);
-							control.getWithdrawBTN().setDisable(true);
-							control.getHistoryBTN().setDisable(true);
-						}
-						if (account.Status==Account.SAVEDACCOUNT) {
-							control.getMode().setText("Saved Account");
-						}else {
-							control.getMode().setText("Ongoing Account");
-						}
-						control.getCardNumber().setText(account.BIC);
-						control.getCvv2().setText(account.CVV2);
-						control.getExp().setText(account.ExDate.getYear() + "/" + account.ExDate.getMonthValue());
-						control.getIban().setText(account.IBAN);
+			if (currentUser.AccessLevel == User.CLIENT) {
+				try {
+					AnchorPane cardPage = loader.load();
+					AnchorPane card = (AnchorPane) cardPage.getChildren().get(0);
 
-						control.getCardNumberTXF().setText(account.BIC);
-						control.getCvv2TXF().setText(account.CVV2);
-						control.getCvvTXF().setText(account.CVV);
-						control.getExpTXF().setText(account.ExDate.getYear() + "/" + account.ExDate.getMonthValue());
-						MainPanel.getChildren().add(parent);
-					} catch (IOException e1) {
-						e1.printStackTrace();
+					cardCreator creator = loader.getController();
+					creator.getCardNumber().setText(account.BIC);
+					creator.getCvv2().setText(account.CVV2);
+					creator.getExp().setText(account.ExDate.getYear() + "/" + account.ExDate.getMonthValue());
+					creator.getIban().setText(account.IBAN);
+					card.setLayoutX((25 + (i % 2) * 420));
+					card.setLayoutY((25 + i / 2 * 225));
+					card.setCursor(Cursor.HAND);
+					card.setOnMouseClicked(e -> {
+						FXMLLoader load = new FXMLLoader(getClass().getResource("../view/AccountInformation.fxml"));
+						Parent parent;
+						try {
+							parent = load.load();
+							AccountInformation control = load.getController();
+							if (!DBConnector.IsCardAlive(account.ExDate.getYear(), account.ExDate.getMonthValue())) {
+								Alert alert = new Alert(AlertType.ERROR);
+								alert.setTitle("Expired");
+								alert.setHeaderText("Your Card is Expired");
+								alert.show();
+								control.getDepositBTn().setDisable(true);
+								control.getWithdrawBTN().setDisable(true);
+								control.getHistoryBTN().setDisable(true);
+							}
+							if (account.Status == Account.SAVEDACCOUNT) {
+								control.getMode().setText("Saved Account");
+							} else {
+								control.getMode().setText("Ongoing Account");
+							}
+							control.getCardNumber().setText(account.BIC);
+							control.getCvv2().setText(account.CVV2);
+							control.getExp().setText(account.ExDate.getYear() + "/" + account.ExDate.getMonthValue());
+							control.getIban().setText(account.IBAN);
+
+							control.getCardNumberTXF().setText(account.BIC);
+							control.getCvv2TXF().setText(account.CVV2);
+							control.getCvvTXF().setText(account.CVV);
+							control.getExpTXF()
+									.setText(account.ExDate.getYear() + "/" + account.ExDate.getMonthValue());
+							MainPanel.getChildren().add(parent);
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+					});
+					cardAnchor.getChildren().add(card);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					FXMLLoader l = new FXMLLoader(this.getClass().getResource("../view/components/eachAccount.fxml"));
+					AnchorPane each = l.load();
+					eachAccount controller = l.getController();
+
+					controller.getAccountId().setText(account.AccountID);
+					controller.getOwnerId().setText(account.OwnerID);
+					controller.getBic().setText(account.BIC);
+					controller.getIban().setText(account.IBAN);
+					controller.getAmount1().setText(account.CVV2);
+					switch (account.Status) {
+					case Account.SAVEDACCOUNT:
+						controller.getStatus().setText("Saved");
+						break;
+
+					case Account.OngoingAccount:
+						controller.getStatus().setText("Ongoing");
+						break;
 					}
-				});
-				cardAnchor.getChildren().add(card);
-				i++;
 
-			} catch (IOException e) {
-				e.printStackTrace();
+					each.setLayoutY((double) i * 145);
+					cardAnchor.getChildren().add(each);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
+
+			i++;
 		}
 
 		if (i == 0) {
--- src/controller/EmployeeList.java
+++ src/controller/EmployeeList.java
@@ -1,9 +1,17 @@
 package controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import model.DBConnector;
+import model.User;
 
-public class EmployeeList {
+public class EmployeeList implements Initializable {
 
     @FXML
     private AnchorPane MainPanel;
@@ -11,4 +19,63 @@ public class EmployeeList {
     @FXML
     private AnchorPane cardAnchor;
 
+    private ArrayList<User> allEmployees = new ArrayList<User>();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        allEmployees = DBConnector.getAllUsers(User.EMPLOYEE);
+
+        showAllUsers();
+    }
+
+    private void showAllUsers() {
+        if (allEmployees.size() == 0) {
+            noResults();
+        }
+
+        for (int i = 0; i < allEmployees.size(); i++) {
+            addUser(i);
+        }
+    }
+
+    private void addUser(int index) {
+        User user = allEmployees.get(index);
+
+        AnchorPane pane = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/components/EachUser.fxml"));
+            pane = loader.load();
+
+            EachUser controller = loader.getController();
+
+            controller.getId().setText(user.ID);
+
+            controller.getFirstName().setText(user.FirstName);
+            controller.getLastName().setText(user.LastName);
+
+            controller.getUsename().setText(user.Username);
+            controller.getNationalCode().setText(user.NationalCode);
+
+            controller.getPhone().setText(user.PhoneNumber);
+            controller.getEmail().setText(user.Email);
+
+            // Group group = (Group) pane.getChildren().get(1);
+            // ((Label) group.getChildren().get(1)).setText(String.valueOf(loan.Value) + currency);
+            // ((Label) group.getChildren().get(3)).setText(loan.AccountID);
+
+            AnchorPane.setTopAnchor(pane, (double) (index * 245));
+            cardAnchor.getChildren().add(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void noResults() {
+        try {
+            cardAnchor.getChildren().add(
+                    (AnchorPane) FXMLLoader.load(this.getClass().getResource("../view/components/emptyField.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
